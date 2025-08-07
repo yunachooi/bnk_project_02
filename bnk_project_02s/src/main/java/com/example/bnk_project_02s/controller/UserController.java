da709684c99ff19f7491553ff289dc3ff1a60189
Yamtan1 <gndndk12305@naver.com>
2025-08-07 15:12:12 +0900

    Update UserController.java

--- bnk_project_02s/src/main/java/com/example/bnk_project_02s/controller/UserController.java
+++ bnk_project_02s/src/main/java/com/example/bnk_project_02s/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.bnk_project_02s.dto.UserDto;
